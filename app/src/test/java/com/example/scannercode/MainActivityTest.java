3549f1fb85d0f1d30280ad3d5aa9765b3334a791
maoldo <maoldo>
2019-06-02 05:42:39 +0200

    Initial commit

---
+++ app/src/test/java/com/example/scannercode/MainActivityTest.java
@@ -0,0 +1,28 @@
+package com.example.scannercode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void onResume() {
+    }
+
+    @Test
+    public void onDestroy() {
+    }
+
+    @Test
+    public void onRequestPermissionsResult() {
+    }
+
+    @Test
+    public void handleResult() {
+    }
+}
